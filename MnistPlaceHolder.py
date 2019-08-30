2214ef14bd74592c7d337bab9a631654bc86956a
Hossein Akhlaghpour <hosseinakhlaghpour@gmail.com>
2019-08-30 01:32:09 +0000

    finally some embeddings

--- MnistPlaceHolder.py
+++ MnistPlaceHolder.py
@@ -1,5 +1,3 @@
-
-
 from keras.datasets import mnist
 from keras.models import Model
 from keras.layers import Input, Dense
@@ -8,34 +6,41 @@ import matplotlib.pyplot as plt
 from numpy import ndarray
 
 
-class MnistPlaceHolder():
+def load():
+    """
+    :return: Tuple of Numpy arrays: (x_train, y_train), (x_test, y_test)
+    """
+    return mnist.load_data()
+
+
+class MnistPlaceHolder:
     def __init__(self):
-        (self.x_train, _), (_, _) = self.load()
+        (self.x_train, _), (_, _) = load()
 
-    def load(self):
-        """
-        :param url:
-        :return: Tuple of Numpy arrays: (x_train, y_train), (x_test, y_test)
-        """
-        return mnist.load_data()
-
-    def getModel(self, encoding_dim=256):
-        input_img = Input(shape=(28,28,))
+    @staticmethod
+    def get_model(encoding_dim=256):
+        input_img = Input(shape=(784,))
         # "encoded" is the encoded representation of the input
         encoded = Dense(encoding_dim, activation='relu')(input_img)
         return Model(input_img, encoded)
 
     def plot(self):
         plt.imshow(self.x_train[0])
+        plt.show()
 
-    def getEmbeddings(self):
-        x_train_float32 = self.x_train[0:2].reshape(-1,28,28).astype(np.float32)
-        model = self.getModel()
+    def get_embeddings(self):
+        x_train_float32 = self.x_train[0:2].reshape(2, -1).astype(np.float32)
+        print('x_train flatten shape ', x_train_float32.shape)
+        print('x[0,0:20] ', x_train_float32[0,0:20])
+        model = self.get_model()
         embeddings = model.predict(x_train_float32)
-        print(embeddings.shape)
+        print('embed..[0,0:20] ', embeddings[0,0:20])
+        print("embedding shape ", embeddings.shape)
         return embeddings
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    plt.show()
     myclass = MnistPlaceHolder()
     myclass.plot()
     print('------ Done ----')
--- TestMnistPlaceHolder.py
+++ TestMnistPlaceHolder.py
@@ -3,22 +3,33 @@ from MnistPlaceHolder import MnistPlaceHolder
 import numpy as np
 import tensorflow as tf
 from numpy import dtype
+import warnings
+
+
+def fxn() -> object:
+    warnings.warn("deprecated", DeprecationWarning)
+
+
+with warnings.catch_warnings(record=True) as w:
+    # Cause all warnings to always be triggered.
+    warnings.simplefilter("always")
+    # Trigger a warning.
+    fxn()
+    # Verify some things
+    assert len(w) == 1
+    assert issubclass(w[-1].category, DeprecationWarning)
+    assert "deprecated" in str(w[-1].message)
+
 
 class TestMnistPlaceHolder(TestCase):
 
-    def test_construct(self):
-        myclass = MnistPlaceHolder()
-        self.assertIsNotNone(myclass, 'XXX')
-
-    def test_load(self):
-        myclass = MnistPlaceHolder()
-        (x_train, y_train), (x_test, y_test) = myclass.load('')
-        self.assertIsNotNone(x_train, msg='could not load mnist')
-        self.assertEqual(x_train.shape, (60000, 28, 28))
-        self.assertEqual(x_train.dtype, np.uint8)
-
-    def test_getEmbeddings(self):
-        myclass = MnistPlaceHolder()
-        float32array = myclass.getEmbeddings()
+    def test_get_embeddings(self):
+        instance = MnistPlaceHolder()
+        float32array = instance.get_embeddings()
         self.assertTrue(float32array.dtype, np.float32)
-        
+
+    def test_init(self):
+        instance = MnistPlaceHolder()
+        self.assertIsNotNone(instance.x_train, msg='could not load mnist')
+        self.assertEqual(instance.x_train.shape, (60000, 28, 28))
+        self.assertEqual(instance.x_train.dtype, np.uint8)
