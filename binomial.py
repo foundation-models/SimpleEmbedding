fc0c2f7251b66b13144c4f0b0ed618dc571f754e
Hossein Akhlaghpour <hosseinakhlaghpour@gmail.com>
2019-09-05 04:04:51 +0000

    2 problem solved

--- binomial.py
+++ binomial.py
@@ -10,4 +10,21 @@ def probability_mass_function(n, k, p):
 def cumulative_distribution_function(n, k, p):
     return sum([probability_mass_function(n, i, p) for i in range(k)])
 
-print('{:0.3f}'.format(1 - cumulative_distribution_function(6,3,(1.09/2.09))))
+
+x, n = map(int, input().split())
+p = x / 100
+"""
+https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem
+expected
+0.696
+"""
+#print('{:0.3f}'.format(1 - cumulative_distribution_function(6,3,(1.09/2.09))))
+
+"""
+ https://www.hackerrank.com/challenges/s10-binomial-distribution-2/problem?h_r=next-challenge&h_v=zen
+expected 
+0.891
+0.342
+"""
+print('{:0.3f}'.format(cumulative_distribution_function(n,3,p)))
+print('{:0.3f}'.format(1 - cumulative_distribution_function(n,2,p)))
